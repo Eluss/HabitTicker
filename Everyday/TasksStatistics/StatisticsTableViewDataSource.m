aa292c6c97f69bbe23ba87b949cbbb6605e7f03a
Eluss <eliasz.sawicki@gmail.com>
2015-07-10 22:47:28 +0200

    StatisticsTableViewDataSource refactoring

--- Everyday/TasksStatistics/StatisticsTableViewDataSource.m
+++ Everyday/TasksStatistics/StatisticsTableViewDataSource.m
@@ -27,32 +27,25 @@
     fromDate = [fromDate dateByAddingDays:-3];
     NSDate *toDate = [NSDate date];
 
-    NSMutableArray *tasksDaysArray = [NSMutableArray new];
-    while (fromDate.day <= toDate.day) {
-        NSArray *tasks = [TasksLoader loadTasksForDate:fromDate];
-        if (tasks != nil) {
-            [tasksDaysArray addObject:tasks];
-        }
-        fromDate = [fromDate dateByAddingDays:1];
-    }
+    return [self statisticsFromDate:fromDate toDate:toDate];
 
-    NSMutableDictionary *doneDictionary = [NSMutableDictionary new];
-    NSMutableDictionary *notDoneDictionary = [NSMutableDictionary new];
-    for (NSArray *array in tasksDaysArray) {
-        for (Task *task in array) {
-            if (task.isDone) {
-                NSNumber *taskCount = doneDictionary[task.name];
-                doneDictionary[task.name] = @(taskCount.integerValue + 1);
-            } else {
-                NSNumber *taskCount = notDoneDictionary[task.name];
-                notDoneDictionary[task.name] = @(taskCount.integerValue + 1);
-            }
-        }
-    }
-    NSArray *doneKeys = [NSArray new];
-    NSArray *notDoneKeys = [NSArray new];
-    doneKeys = [doneDictionary allKeys];
-    notDoneKeys = [notDoneDictionary allKeys];
+
+}
+
+- (NSArray *)statisticsFromDate:(NSDate *)fromDate toDate:(NSDate *)toDate {
+    NSMutableArray *tasksDaysArray = [self tasksFromDate:fromDate toDate:toDate];
+
+    NSMutableDictionary *notDoneDictionary = [self notDoneDictionaryFromTasks:tasksDaysArray];
+    NSMutableDictionary *doneDictionary = [self doneDictionaryFromTasks:tasksDaysArray];
+
+    NSMutableArray *resultArray = [self tasksStatisticsWithNotDoneDictionary:notDoneDictionary doneDictionary:doneDictionary];
+
+    return resultArray;
+}
+
+- (NSMutableArray *)tasksStatisticsWithNotDoneDictionary:(NSMutableDictionary *)notDoneDictionary doneDictionary:(NSMutableDictionary *)doneDictionary {
+    NSArray *doneKeys = [doneDictionary allKeys];
+    NSArray *notDoneKeys = [notDoneDictionary allKeys];
 
     NSMutableArray *resultArray = [NSMutableArray new];
     for (NSString *key in doneKeys) {
@@ -72,8 +65,45 @@
             [resultArray addObject:taskStatistics];
         }
     }
-
     return resultArray;
+}
+
+- (NSMutableDictionary *)notDoneDictionaryFromTasks:(NSMutableArray *)tasksDaysArray {
+    NSMutableDictionary *notDoneDictionary = [NSMutableDictionary new];
+    for (NSArray *array in tasksDaysArray) {
+        for (Task *task in array) {
+            if (task.isDone) {
+                NSNumber *taskCount = notDoneDictionary[task.name];
+                notDoneDictionary[task.name] = @(taskCount.integerValue + 1);
+            }
+        }
+    }
+    return notDoneDictionary;
+}
+
+- (NSMutableDictionary *)doneDictionaryFromTasks:(NSMutableArray *)tasksDaysArray {
+    NSMutableDictionary *doneDictionary = [NSMutableDictionary new];
+    for (NSArray *array in tasksDaysArray) {
+        for (Task *task in array) {
+            if (task.isDone) {
+                NSNumber *taskCount = doneDictionary[task.name];
+                doneDictionary[task.name] = @(taskCount.integerValue + 1);
+            }
+        }
+    }
+    return doneDictionary;
+}
+
+- (NSMutableArray *)tasksFromDate:(NSDate *)fromDate toDate:(NSDate *)toDate {
+    NSMutableArray *tasksDaysArray = [NSMutableArray new];
+    while (fromDate.day <= toDate.day) {
+        NSArray *tasks = [TasksLoader loadTasksForDate:fromDate];
+        if (tasks != nil) {
+            [tasksDaysArray addObject:tasks];
+        }
+        fromDate = [fromDate dateByAddingDays:1];
+    }
+    return tasksDaysArray;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
